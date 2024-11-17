8381665293b8d6106a58f311e3b44f3c9f16850f
omidrm99 <omidrm99@gmail.com>
2024-11-17 20:19:20 +0330

    single page for showing individual jobs

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -22,9 +22,9 @@ class JobController extends Controller
         //
     }
 
-    public function show(string $id)
+    public function show(Job $job)
     {
-        //
+        return view('job.show' , ['job' => $job]);
     }
 
     public function edit(string $id)
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,17 +1,17 @@
 <x-layout>
     @foreach($jobs as $job)
         <x-card class="mb-2">
-            <div class="flex justify-between mb-4">
+            <div class="flex items-center justify-between mb-4">
                 <h2 class="text-lg font-medium"> {{ $job->title }} </h2>
                 <div class="text-slate-500"> ${{number_format($job->salary) }} </div>
             </div>
 
             <div class="mb-4 flex justify-between text-sm text-slate-500 items-center">
-                <div class="flex space-x-4">
+                <div class="flex items-center space-x-4">
                     <div>company name</div>
                     <div>{{ $job->location }}</div>
                 </div>
-                <div class="flex space-x-2 text-xs">
+                <div class="flex items-center space-x-2 text-xs">
                     <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
                     <x-tag>{{ $job->category }}</x-tag>
                 </div>
