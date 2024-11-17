d9ac6a4dbb9b27dcfb433345c35c4dec75fc4cce
omidrm99 <omidrm99@gmail.com>
2024-11-17 22:04:45 +0330

    nav bar created and made as component

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4"
+                   :links="['jobs' => route('jobs.index')]"></x-breadcrumbs>
     @foreach($jobs as $job)
         <x-job-card class="mb-2" :job="$job">
             <div>
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-breadcrumbs class="mb-4"
-        :links="['jobs' => route('jobs.index'), $job->title => '#']"></x-breadcrumbs>
+                   :links="['jobs' => route('jobs.index'), $job->title => '#']"></x-breadcrumbs>
     <x-job-card :job="$job"></x-job-card>
 </x-layout>
 
